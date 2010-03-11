8ce844eaf0485832b4fb7a8d639bf969ca462045
Zach Moazeni <zach.moazeni@gmail.com>
2010-03-10 20:38:44 -0500

    Adding check if the credentials aren't in place

--- features/step_definitions/harvest_steps.rb
+++ features/step_definitions/harvest_steps.rb
@@ -1,4 +1,5 @@
 Given /^I am using the credentials from "([^\"]*)"$/ do |path|
+  raise "You need a credentials file in support/harvest_credentials.yml!!" unless File.exist?("#{File.dirname(__FILE__)}/../#{path}")
   credentials = YAML.load_file("#{File.dirname(__FILE__)}/../#{path}")
   @username = credentials["username"]
   @password = credentials["password"]
