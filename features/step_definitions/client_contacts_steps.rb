8badb8f207aca5430b88da7b22fff78ac8a832c6
Zach Moazeni <zach.moazeni@gmail.com>
2010-03-20 21:36:50 -0400

    Doing some reorganizing

--- features/step_definitions/client_contacts_steps.rb
+++ features/step_definitions/client_contacts_steps.rb
@@ -1,10 +1,10 @@
-When /^I create a contact for the client named "([^\"]*)" with the following:$/ do |name, table|
+When 'I create a contact for the client named "$1" with the following:' do |name, table|
   client = Then %Q{there should be a client with the name "#{name}"}
   contact = harvest_api.contacts.create(table.rows_hash.merge("client_id" => client.id))
   contact.first_name.should == table.rows_hash["first_name"]
 end
 
-Then /^there should be a contact for the client named "([^\"]*)" with an email address "([^\"]*)"$/ do |name, email|
+Then 'there should be a contact for the client named "$1" with an email address "$2"' do |name, email|
   client = Then %Q{there should be a client with the name "#{name}"}
   
   contacts = harvest_api.contacts.all
@@ -14,4 +14,11 @@ Then /^there should be a contact for the client named "([^\"]*)" with an email a
   contacts = harvest_api.contacts.all(client.id)
   contact2 = contacts.detect {|c| c.email == email}
   contact2.should == contact
+  contact
+end
+
+When 'I update the contact for the client named "$1" with the email address "$2" with the following:' do |name, email, table|
+  pending
+  contact = Then 'there should be a contact for the client named "#{name}" with an email address "#{email}"'
+  harvest_api.contacts.update(contact.id, table.row_hashes)
 end
