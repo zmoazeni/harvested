7f4d8ae29104bee51d43cf26796a3aa3bd05f7da
Zach Moazeni <zach.moazeni@gmail.com>
2010-03-20 21:32:12 -0400

    Renaming the step files and converting regexs to strings

--- features/step_definitions/client_contacts_steps.rb
+++ features/step_definitions/client_contacts_steps.rb
@@ -1,13 +1,11 @@
 When /^I create a contact for the client named "([^\"]*)" with the following:$/ do |name, table|
-  clients = harvest_api.clients.all
-  client = clients.detect {|c| c.name == name}
+  client = Then %Q{there should be a client with the name "#{name}"}
   contact = harvest_api.contacts.create(table.rows_hash.merge("client_id" => client.id))
   contact.first_name.should == table.rows_hash["first_name"]
 end
 
 Then /^there should be a contact for the client named "([^\"]*)" with an email address "([^\"]*)"$/ do |name, email|
-  clients = harvest_api.clients.all
-  client = clients.detect {|c| c.name == name}
+  client = Then %Q{there should be a client with the name "#{name}"}
   
   contacts = harvest_api.contacts.all
   contact = contacts.detect {|c| c.email == email}
--- features/step_definitions/clients_steps.rb
+++ features/step_definitions/clients_steps.rb
@@ -1,42 +1,40 @@
-When /^I create a client with the following:$/ do |table|
+When 'I create a client with the following:' do |table|
   client = harvest_api.clients.create(table.rows_hash)
   client.name.should == table.rows_hash["name"]
 end
 
-Then /^there should be a client with the name "([^\"]*)"$/ do |name|
-  clients = harvest_api.clients.all
-  clients.detect {|c| c.name == name}.should_not be_nil
-end
-
-Then /^I should be able to retrieve the client named "([^\"]*)" by id$/ do |name|
+Then 'there should be a client with the name "$1"' do |name|
   clients = harvest_api.clients.all
   client = clients.detect {|c| c.name == name}
+  client.should_not be_nil
+  client
+end
+
+Then 'there should not be a client with the name "$1"' do |name|
+  clients = harvest_api.clients.all
+  clients.detect {|c| c.name == name}.should be_nil
+end
+
+Then 'I should be able to retrieve the client named "$1" by id' do |name|
+  client = Then %Q{there should be a client with the name "#{name}"}
   harvest_api.clients.find(client.id).should == client
 end
 
-When /^I update the client named "([^\"]*)" with the following details "([^\"]*)"$/ do |name, details|
-  clients = harvest_api.clients.all
-  client = clients.detect {|c| c.name == name}
+When 'I update the client named "$1" with the following details "$2"' do |name, details|
+  client = Then %Q{there should be a client with the name "#{name}"}
   updated_client = harvest_api.clients.update(client.id, "details" => details)
   updated_client.name.should == name
 end
 
-Then /^the details of "([^\"]*)" should be "([^\"]*)"$/ do |name, details|
-  clients = harvest_api.clients.all
-  client = clients.detect {|c| c.name == name}
+Then 'the details of "$1" should be "$2"' do |name, details|
+  client = Then %Q{there should be a client with the name "#{name}"}
   client.details.should == details
 end
 
-When /^I delete the client named "([^\"]*)"$/ do |name|
-  clients = harvest_api.clients.all
-  client = clients.detect {|c| c.name == name}
+When 'I delete the client named "$1"' do |name|
+  client = Then %Q{there should be a client with the name "#{name}"}
   id = harvest_api.clients.delete(client.id)
   id.should == client.id
-end
-
-Then /^there should not be a client with the name "([^\"]*)"$/ do |name|
-  clients = harvest_api.clients.all
-  clients.detect {|c| c.name == name}.should be_nil
 end
 
 Then /^the client named "([^\"]*)" should be activated$/ do |name|
