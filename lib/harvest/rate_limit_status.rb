83c5bc9c256101026321f380cd41976eb1c253e0
Zach Moazeni <zach.moazeni@gmail.com>
2010-04-10 00:52:18 -0400

    Changing DateTime to Time

--- lib/harvest/rate_limit_status.rb
+++ lib/harvest/rate_limit_status.rb
@@ -3,7 +3,7 @@ module Harvest
     include HappyMapper
     
     tag 'hash'
-    element :last_access_at, DateTime, :tag => 'last-access-at'
+    element :last_access_at, Time, :tag => 'last-access-at'
     element :count, Integer
     element :timeframe_limit, Integer, :tag => 'timeframe-limit'
     element :max_calls, Integer, :tag => 'max-calls'
