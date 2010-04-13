5948d2f5bd3b27354d9361801a5e52c2eb121af2
Zach Moazeni <zach.moazeni@gmail.com>
2010-04-12 23:53:26 -0400

    Documenting Activatable

--- lib/harvest/behavior/activatable.rb
+++ lib/harvest/behavior/activatable.rb
@@ -1,6 +1,12 @@
 module Harvest
   module Behavior
+    
+    # Activate/Deactivate behaviors that can be brought into API collections
     module Activatable
+      # Deactivates the item. Does nothing if the item is already deactivated
+      # 
+      # @param [Harvest::BaseModel] model the model you want to deactivate
+      # @return [Harvest::BaseModel] the deactivated model
       def deactivate(model)
         if model.active?
           request(:post, credentials, "#{api_model.api_path}/#{model.to_i}/toggle")
@@ -9,6 +15,10 @@ module Harvest
         model
       end
     
+      # Activates the item. Does nothing if the item is already activated
+      # 
+      # @param [Harvest::BaseModel] model the model you want to activate
+      # @return [Harvest::BaseModel] the activated model
       def activate(model)
         if !model.active?
           request(:post, credentials, "#{api_model.api_path}/#{model.to_i}/toggle")
