819b5e2496b1a6e3a31eb1cc70514cafa02e1401
Zach Moazeni <zach.moazeni@gmail.com>
2010-04-22 00:05:08 -0400

    Documenting reset_password

--- lib/harvest/api/users.rb
+++ lib/harvest/api/users.rb
@@ -5,7 +5,13 @@ module Harvest
     
       include Harvest::Behavior::Crud
       include Harvest::Behavior::Activatable
-    
+      
+      # Triggers Harvest to reset the user's password and sends them an email to change it.
+      # @overload reset_password(id)
+      #   @param [Integer] id the id of the user you want to reset the password for
+      # @overload reset_password(user)
+      #   @param [Harvest::User] user the user you want to reset the password for
+      # @return [Harvest::User] the user you passed in
       def reset_password(user)
         request(:post, credentials, "#{api_model.api_path}/#{user.to_i}/reset_password")
         user
