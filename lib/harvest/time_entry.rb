c447190af7344200cd7ffddf43391c8bdc117003
Michelle Moon Lee <michelle@quilted.coop>
2010-10-19 20:09:27 -0400

    Added of_user element to time_entry and added it to the to_xml function

--- lib/harvest/time_entry.rb
+++ lib/harvest/time_entry.rb
@@ -17,6 +17,7 @@ module Harvest
     element :created_at, Time
     element :updated_at, Time
     element :user_id, Integer
+    element :of_user, Integer
     element :closed, Boolean, :tag => 'is-closed'
     element :billed, Boolean, :tag => 'is-billed'
     
@@ -32,6 +33,7 @@ module Harvest
         r.tag!('project_id', project_id) if project_id
         r.tag!('task_id', task_id) if task_id
         r.tag!('spent_at', spent_at) if spent_at
+        r.tag!('of_user', of_user) if of_user
       end
     end
     
