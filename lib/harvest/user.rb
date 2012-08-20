3e5c6e4e0f393baf4596e7dea87ca67449260edb
Mikel Lindsaar <mikel@reinteractive.net>
2012-08-21 07:54:21 +1000

    Updating user model to include new property introduced by Harvest

--- lib/harvest/user.rb
+++ lib/harvest/user.rb
@@ -1,6 +1,7 @@
 module Harvest
-  
-  # The model that contains information about a task
+
+  # The model that contains information about a user of the system which is
+  # accessed via the /people API call in the Harvest API.
   #
   # == Fields
   # [+id+] (READONLY) the id of the user
@@ -17,61 +18,63 @@ module Harvest
   # [+timezone+] the timezone for the user.
   class User < Hashie::Dash
     include Harvest::Model
-    
+
     api_path '/people'
-    property :id
-    property :email
-    property :first_name
-    property :last_name
-    property :has_access_to_all_future_projects
+    property :cache_version
+    property :created_at
+    property :default_expense_category_id
+    property :default_expense_project_id
     property :default_hourly_rate
+    property :default_task_id
+    property :default_time_project_id
+    property :department
+    property :duplicate_timesheet_wants_notes
+    property :email
+    property :email_after_submit
+    property :first_name
+    property :first_timer
+    property :has_access_to_all_future_projects
+    property :id
+    property :identity_url
     property :is_active
     property :is_admin
     property :is_contractor
-    property :telephone
-    property :department
-    property :timezone
-    property :password
-    property :first_timer
-    property :wants_newsletter
-    property :preferred_project_status_reports_screen
-    property :preferred_approval_screen
-    property :created_at
-    property :updated_at
-    property :twitter_username
-    property :preferred_entry_method
-    property :default_time_project_id
-    property :default_task_id
-    property :default_expense_category_id
+    property :last_name
     property :opensocial_identifier
-    property :duplicate_timesheet_wants_notes
-    property :wants_timesheet_duplication
-    property :cache_version
-    property :email_after_submit
-    property :default_expense_project_id
-    property :identity_url
+    property :password
+    property :password_change_required
+    property :preferred_approval_screen
+    property :preferred_entry_method
+    property :preferred_project_status_reports_screen
+    property :telephone
     property :timestamp_timers
-    property :weekly_digest_sent_on
+    property :timezone
+    property :twitter_username
+    property :updated_at
+    property :wants_newsletter
+    property :wants_timesheet_duplication
     property :wants_weekly_digest
-    
+    property :weekly_digest_sent_on
+
     alias_method :active?, :is_active
     alias_method :admin?, :is_admin
     alias_method :contractor?, :is_contractor
-    
+    alias_method :password_change_required?, :password_change_required
+
     def initialize(args = {})
       args          = args.stringify_keys
       args["is_admin"] = args.delete("admin") if args["admin"]
       self.timezone = args.delete("timezone") if args["timezone"]
       super
     end
-    
+
     # Sets the timezone for the user. This can be done in a variety of ways.
-    # 
+    #
     # == Examples
     #   user.timezone = :cst # the easiest way. CST, EST, MST, and PST are supported
-    #   
+    #
     #   user.timezone = 'america/chicago' # a little more verbose
-    #   
+    #
     #   user.timezone = 'Central Time (US & Canada)' # the most explicit way
     def timezone=(timezone)
       tz = timezone.to_s.downcase
