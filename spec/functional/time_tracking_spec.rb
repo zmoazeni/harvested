b3431fe2d6effd34f78411d9ac1ab32846f2c680
Zach Moazeni <zach.moazeni@gmail.com>
2012-08-22 22:16:28 -0400

    Changes timestamp to 1.9.x format

--- spec/functional/time_tracking_spec.rb
+++ spec/functional/time_tracking_spec.rb
@@ -7,23 +7,23 @@ describe 'harvest time tracking' do
       project = harvest.projects.create("name" => "Tracking Project", "client_id" => client.id)
       harvest.projects.create_task(project, "A billable task")
       task = harvest.tasks.all.detect {|t| t.name == "A billable task"}
-      
-      entry = harvest.time.create("notes" => "Test api support", "hours" => 3, "spent_at" => "12/28/2009", "task_id" => task.id, "project_id" => project.id)
+
+      entry = harvest.time.create("notes" => "Test api support", "hours" => 3, "spent_at" => "2009/12/28", "task_id" => task.id, "project_id" => project.id)
       entry.notes.should == "Test api support"
-      
+
       entry.notes = "Upgraded to JSON"
       entry = harvest.time.update(entry)
       entry.notes.should == "Upgraded to JSON"
-      
+
       harvest.time.delete(entry)
       harvest.time.all(Time.utc(2009, 12, 28)).should == []
     end
   end
-  
+
   it 'allows you to save entries for a different user' do
     cassette("time_tracking2") do
       user = harvest.users.create(
-        "email"      => "frank@example.com", 
+        "email"      => "frank@example.com",
         "first_name" => "Frank",
         "last_name"  => "Doe",
         "password"   => "secure"
@@ -33,21 +33,21 @@ describe 'harvest time tracking' do
       harvest.user_assignments.create("project" => project, "user" => user)
       harvest.projects.create_task(project, "A billable task for tuxes")
       task = harvest.tasks.all.detect {|t| t.name == "A billable task for tuxes"}
-      
-      entry = harvest.time.create("notes" => "Test api support", "hours" => 3, "spent_at" => "12/28/2009", "task_id" => task.id, "project_id" => project.id, "of_user" => user.id)
+
+      entry = harvest.time.create("notes" => "Test api support", "hours" => 3, "spent_at" => "2009/12/28", "task_id" => task.id, "project_id" => project.id, "of_user" => user.id)
       harvest.time.all(Time.utc(2009, 12, 28), user).should == [entry]
-      
+
       entry.notes = "Updating notes"
       entry = harvest.time.update(entry, user)
       entry.notes.should == "Updating notes"
-      
+
       entry = harvest.time.find(entry, user)
       entry.notes.should == "Updating notes"
-      
+
       harvest.time.delete(entry, user)
       harvest.time.all(Time.utc(2009, 12, 28), user).should == []
     end
   end
-  
+
   it 'allows toggling of timers'
 end
