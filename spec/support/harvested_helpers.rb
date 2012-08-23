f4b5804eb9f157d48d44c2746031fad0145d17ce
Zach Moazeni <zach.moazeni@gmail.com>
2012-08-22 21:59:45 -0400

    Bumps up timestamp to 2013
    I need to find a more permanent solution. Perhaps 1 year past today.

--- spec/support/harvested_helpers.rb
+++ spec/support/harvested_helpers.rb
@@ -4,33 +4,33 @@ module HarvestedHelpers
     @credentials ||= YAML.load_file("#{File.dirname(__FILE__)}/harvest_credentials.yml")
   end
   def credentials; HarvestedHelpers.credentials; end
-  
+
   def self.simple_harvest
     Harvest.client(credentials["subdomain"], credentials["username"], credentials["password"], :ssl => true)
   end
-  
+
   # def connect_to_harvest
   #   @harvest = Harvest.hardy_client(credentials["subdomain"], credentials["username"], credentials["password"], :ssl => true)
   # end
-  
+
   # def harvest; @harvest; end
   def harvest; @harvest ||= HarvestedHelpers.simple_harvest; end
-  
+
   def hardy_harvest
     Harvest.hardy_client(credentials["subdomain"], credentials["username"], credentials["password"], :ssl => true)
   end
-  
+
   def self.clean_remote
     harvest = simple_harvest
     harvest.users.all.each do |u|
-      harvest.reports.expenses_by_user(u, Time.utc(2000, 1, 1), Time.utc(2011, 6,21)).each do |expense|
+      harvest.reports.expenses_by_user(u, Time.utc(2000, 1, 1), Time.utc(2013, 6,21)).each do |expense|
         harvest.expenses.delete(expense, u)
       end
-      
-      harvest.reports.time_by_user(u, Time.utc(2000, 1, 1), Time.utc(2011, 6,21)).each do |time|
+
+      harvest.reports.time_by_user(u, Time.utc(2000, 1, 1), Time.utc(2013, 6,21)).each do |time|
         harvest.time.delete(time, u)
       end
-      
+
       harvest.users.delete(u) if u.email != credentials["username"]
     end
 
