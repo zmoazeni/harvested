738639ccbf64f4307ea6278139c26a12fed40298
Jeffrey Lee <jlee@lsu.edu>
2012-09-13 12:32:01 -0500

    Restores expense and time_entry customizations.

--- lib/harvest/time_entry.rb
+++ lib/harvest/time_entry.rb
@@ -8,8 +8,18 @@ module Harvest
 
     def initialize(args = {}, _ = nil)
       args = args.to_hash.stringify_keys
+      self.spent_at = args.delete("spent_at") if args["spent_at"]
       super
     end
 
+    def spent_at=(date)
+      self["spent_at"] = (String === date ? Time.parse(date) : date)
+    end
+
+    def as_json(args = {})
+      super(args).to_hash.stringify_keys.tap do |hash|
+        hash.update("spent_at" => (spent_at.nil? ? nil : spent_at.to_time.to_s))
+      end
+    end
   end
 end
--- spec/harvest/time_entry_spec.rb
+++ spec/harvest/time_entry_spec.rb
@@ -3,8 +3,8 @@ require 'spec_helper'
 describe Harvest::TimeEntry do
   context '#as_json' do
     it 'builds a specialized hash' do
-      entry = Harvest::TimeEntry.new(:notes => 'the notes', :project_id => 'the project id', :task_id => 'the task id', :hours => 'the hours', :spent_at => Time.utc(2009, 12, 28))
-      entry.as_json.should == {"notes" => "the notes", "hours" => "the hours", "project_id" => "the project id", "task_id" => "the task id", "spent_at" => "2009-12-28T00:00:00Z"}
+      entry = Harvest::TimeEntry.new(:spent_at => Time.utc(2009, 12, 28), :notes => 'the notes', :project_id => 'the project id', :task_id => 'the task id', :hours => 'the hours')
+      entry.as_json.should == {"spent_at" => Time.utc(2009, 12, 28).to_s, "notes" => "the notes", "hours" => "the hours", "project_id" => "the project id", "task_id" => "the task id"}
     end
   end
   
