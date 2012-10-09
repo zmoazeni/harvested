2dac2a8498c45a3b878658817d31886f5480cd17
Mark Rickert <mjar81@gmail.com>
2012-10-09 15:34:29 -0400

    Removed extraneous ```puts``` calls.

--- examples/project_create_script.rb
+++ examples/project_create_script.rb
@@ -6,8 +6,7 @@ password = 'yourpassword'
 
 harvest = Harvest.hardy_client(subdomain, username, password)
 
-puts
-puts "Please search for a client by typing part of their name:"
+puts "\nPlease search for a client by typing part of their name:"
 cname = gets.chomp
 
 # Filter out all the clients that don't match the typed string
@@ -15,48 +14,38 @@ client_search_results = harvest.clients.all.select { |c| c.name.downcase.include
 
 case client_search_results.length
 when 0
-	puts "No client found.  Please try again."
-	puts
+	puts "No client found.  Please try again.\n"
 	abort
 when 1
 	# Result is exactly 1. We got the client.
 	client_index = 0
 when 1..15
 	# Have the user select from a list of clients.
-	puts "Please select from the following results:"
-	puts
+	puts "Please select from the following results:\n"
 	client_search_results.each_with_index do |c, i|
 		puts "  #{i+1}. #{c.name}"
 	end
 
 	client_index = gets.chomp.to_i - 1	
 else
-	puts "Too many client matches. Please try a more specific search term."
-	puts
+	puts "Too many client matches. Please try a more specific search term.\n"
 	abort
 end
 
 client = client_search_results[client_index]
 
-puts
-puts "Client found: #{client.name}"
-puts
-
+puts "\nClient found: #{client.name}\n"
 puts "Please enter the project name:"
 pname = gets.chomp
-puts
 
-puts "Is this project billable? (y/n)"
+puts "\nIs this project billable? (y/n)"
 billable = gets.chomp.downcase == "y"
-puts
 
-puts "Any project notes? (hit 'return' for none)"
+puts "\nAny project notes? (hit 'return' for none)"
 notes = gets.chomp
-puts
 
-puts "What sorts of tasks does this project need? Type \"p\" for project tasks or \"s\" for sales tasks."
+puts "\nWhat sorts of tasks does this project need? Type \"p\" for project tasks or \"s\" for sales tasks.\n"
 task_types = gets.chomp
-puts
 
 # Determine what task list to use based on the project type.
 if task_types.downcase == "p"
@@ -70,8 +59,7 @@ end
 tasks = harvest.tasks.all.select { |t| use_tasks.include?(t.name) }
 
 # Create the project
-puts "Creating new project: \"#{pname}\" for client: #{client.name}"
-puts
+puts "Creating new project: \"#{pname}\" for client: #{client.name}\n"
 project = Harvest::Project.new(:name => pname, :client_id => client.id, :billable => billable, :notes => notes)
 project = harvest.projects.create(project)
 
@@ -92,9 +80,6 @@ harvest.users.all.each do |u|
 	harvest.user_assignments.create(user_assignment)
 end
 
-puts
-puts "Project successfully created."
-puts "You can find the project here: http://#{subdomain}.harvestapp.com/projects/#{project.id}/edit"
-puts
-puts
+puts "\nProject successfully created."
+puts "You can find the project here: http://#{subdomain}.harvestapp.com/projects/#{project.id}/edit\n\n"
 
