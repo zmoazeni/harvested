2b93c2868110d393c3c955ff1cc95fb48628749e
Zach Moazeni <zach.moazeni@gmail.com>
2014-08-29 16:23:05 -0400

    Fixes deprecation warning

--- spec/harvest/task_assignment_spec.rb
+++ spec/harvest/task_assignment_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Harvest::TaskAssignment do
   context "task_as_json" do
     it "generates the json for existing tasks" do
-      assignment = Harvest::TaskAssignment.new(:task => mock(:task, :to_i => 3))
+      assignment = Harvest::TaskAssignment.new(:task => double(:task, :to_i => 3))
       assignment.task_as_json.should == {"task" => {"id" => 3}}
     end
   end
--- spec/harvest/user_assignment_spec.rb
+++ spec/harvest/user_assignment_spec.rb
@@ -3,7 +3,7 @@ require 'spec_helper'
 describe Harvest::UserAssignment do
   describe "#user_as_json" do
     it "should generate the xml for existing users" do
-      assignment = Harvest::UserAssignment.new(:user => mock(:user, :to_i => 3))
+      assignment = Harvest::UserAssignment.new(:user => double(:user, :to_i => 3))
       assignment.user_as_json.should == {"user" => {"id" => 3}}
     end
   end
