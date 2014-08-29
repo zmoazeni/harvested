29a3f8ed039109ca306c2da1dcafb92a6834605c
Zach Moazeni <zach.moazeni@gmail.com>
2014-08-29 16:17:35 -0400

    The throttling limit changed

--- spec/functional/account_spec.rb
+++ spec/functional/account_spec.rb
@@ -4,10 +4,10 @@ describe 'account information' do
   it 'returns the rate limit when requested' do
     cassette('account1') do
       status = harvest.account.rate_limit_status
-      status.max_calls.should == 500
+      status.max_calls.should == 100
     end
   end
-  
+
   it 'returns the result of whoami' do
     cassette('account2') do
       user = harvest.account.who_am_i
