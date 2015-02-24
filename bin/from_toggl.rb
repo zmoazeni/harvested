e774bc4de9e65bab282f4f63c6c69cafc5a01eb0
Peter Burkholder <peterb@getchef.com>
2015-02-23 20:25:09 -0500

    Builds Arr of entries

--- bin/from_toggl.rb
+++ bin/from_toggl.rb
@@ -7,11 +7,11 @@ class String
 end
 
 CsvEntry = Struct.new(
-  :client,
-  :project,
-  :task,
-  :description,
-  :date,
+  :client,     # 2
+  :project,    # 3
+  :task,       # 4
+  :description,# 5
+  :date,       # 7
   :duration
 )
 
@@ -20,13 +20,23 @@ def duration_to_sec(duration)
   in_secs =  hms[0].to_i * 3600 + hms[1].to_i * 60 + hms[1].to_i
 end
 
-
-
 current_dir = File.dirname(__FILE__)
+entries = Array.new
 
 toggl_file = current_dir + '/../Toggl_time_entries_2015-02-16_to_2015-02-22.csv'
 CSV.foreach(toggl_file) do |row|
-  duration = row[-3]
-  next unless duration.match(/..:..:../)
-  p duration_to_sec(duration)
+  duration_entry = row[-3]
+  next unless duration_entry.match(/..:..:../)
+  duration = duration_to_sec(duration_entry)
+  next unless duration > 119
+  c = CsvEntry.new()
+  c.duration = duration
+  c.client = row[2]
+  c.project = row[3]
+  c.task = row[4]
+  c.description = row[5]
+  c.date = row[7]
+  entries << c
 end
+
+p entries[-1]
