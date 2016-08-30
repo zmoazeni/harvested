e0839dd61589bce608bc68257bc370a8a906c652
Sergeev Peter <sergeev.peter@gmail.com>
2016-08-30 15:04:51 +0300

    Make expenses attachments work

--- lib/harvest/api/expenses.rb
+++ lib/harvest/api/expenses.rb
@@ -2,25 +2,31 @@ module Harvest
   module API
     class Expenses < Base
       api_model Harvest::Expense
-      
+
       include Harvest::Behavior::Crud
-      
+
       def all(date = ::Time.now, user = nil)
         date = ::Time.parse(date) if String === date
         response = request(:get, credentials, "#{api_model.api_path}/#{date.yday}/#{date.year}", :query => of_user_query(user))
         api_model.parse(response.parsed_response)
       end
-      
-      # This is currently broken, but will come back to it
+
       def attach(expense, filename, receipt)
         body = ""
-        body << "------------------------------b7edea381b46\r\n"
+        body << "--__X_ATTACH_BOUNDARY__\r\n"
         body << %Q{Content-Disposition: form-data; name="expense[receipt]"; filename="#{filename}"\r\n}
-        body << "Content-Type: image/png\r\n"
-        body << "\r\n#{receipt.read}\r\n"
-        body << "------------------------------b7edea381b46\r\n"
-        
-        request(:post, credentials, "#{api_model.api_path}/#{expense.to_i}/receipt", :headers => {'Content-Type' => 'multipart/form-data; boundary=------------------------------b7edea381b46'}, :body => body)
+        body << "\r\n#{receipt.read}"
+        body << "\r\n--__X_ATTACH_BOUNDARY__--\r\n\r\n"
+
+        request(
+          :post,
+          credentials,
+          "#{api_model.api_path}/#{expense.to_i}/receipt",
+          :headers => {
+            'Content-Type' => 'multipart/form-data; charset=utf-8; boundary=__X_ATTACH_BOUNDARY__',
+            'Content-Length' => body.length.to_s,
+          },
+          :body => body)
       end
     end
   end
