371f94d85d3aeb28ca41753247bffb698b95e114
Zach Moazeni <zach.moazeni@gmail.com>
2017-04-21 10:10:08 -0400

    Fixes invoice category issue
    It requires "invoice_category_item" when we send it updates, but it is returned as "invoice_items"

--- lib/harvest/invoice_category.rb
+++ lib/harvest/invoice_category.rb
@@ -3,7 +3,13 @@ module Harvest
     include Harvest::Model
 
     api_path '/invoice_item_categories'
-    def self.json_root; "category"; end
+    def self.json_root; "invoice_item_category"; end
 
+    class << self
+      def parse(json)
+        parsed = String === json ? JSON.parse(json) : json
+        Array.wrap(parsed).map {|attrs| new(attrs["invoice_category"])}
+      end
+    end
   end
 end
